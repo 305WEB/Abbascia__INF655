0c102c3193346fe03df353198a97ea4d02c2bc05
Claudio Abbascia <responsiveweb@outlook.com>
2022-01-29 13:16:20 -0500

    Commit 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+// import React from 'react';  // Not neede when using JSX
 
 function App() {
+
+  const name = "Claudio";
+
+  const tasks = [
+    {
+      id: 1,
+      title: "Office Work",
+      description: "Setup GITHUB"
+    },
+
+    {
+      id: 1,
+      title: "Work",
+      description: "Setup oFFICE"
+    },
+    {
+      id: 1,
+      title: "Today Work",
+      description: "Setup oFFICE"
+    },
+
+  ];
+
+const taskManager = () => {
+  const init = Math.floor(Math.random() * 3);
+  return tasks[init].title;
+
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">  
+    
+     <h1>First React App</h1>
+
+     <p>Hello: {name},tomorrow: {tasks[1].title}</p>
+
+     <p>Hello: {name},tomorrow your task is: {taskManager()}</p>
+
     </div>
   );
 }
+
+// function App() { // Example not using JSX
+
+// return React.createElement("div", {id: "hello", className: "Test"}, React.createElement("h1", null, "Hello World"));
+
+// }
 
 export default App;
